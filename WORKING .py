ada7f27192b24ee43f1f6c1c929e4a1ce05ae763
morrisj70800 <78512092+morrisj70800@users.noreply.github.com>
2021-02-12 10:44:17 +1300

    temperature

---
+++ WORKING .py
@@ -0,0 +1,33 @@
+from tkinter import *
+from functools import partial   # To prevent unwanted windows
+
+import random
+
+
+class Converter:
+    def __init__(self):
+
+        # Formatting variables...
+        background_color = "light blue"
+
+        # Converter Main Screen GUI...
+        self.converter_frame = Frame(width=300, height=300, bg=background_color)
+        self.converter_frame.grid()
+
+        # Temperature Conversion Heading (row 0)
+        self.temp_converter_label = Label(text="Temperature Converter",
+                                          font=("arial", "16", "bold"),
+                                          bg=background_color,
+                                          padx=10, pady=10)
+        self.temp_converter_label.grid(row=0)
+
+
+
+
+
+
+
+
+
+
+
