d431696a31adce65c7d05300e3dbbab49cd42001
morrisj70800 <78512092+morrisj70800@users.noreply.github.com>
2021-02-22 09:21:02 +1300

    Temp Converter

--- CONVERTER.py
+++ CONVERTER.py
@@ -44,12 +44,14 @@ class Converter:
 
         self.to_c_button = Button(self.conversion_buttons_frame,
                                   text="To Centigrade", font="Arial 10 bold",
-                                  bg="Khaki1", padx=10, pady=10,)
+                                  bg="Khaki1", padx=10, pady=10,
+                                  command=lambda: self.temp_convert(-459))
         self.to_c_button.grid(row=0, column=0)
 
         self.to_f_button = Button(self.conversion_buttons_frame,
                                   text="To Fahrenheit", font="arial 10 bold",
-                                  bg="orchid1", padx=10, pady=10)
+                                  bg="orchid1", padx=10, pady=10,
+                                  command=lambda: self.temp_convert(-273))
         self.to_f_button.grid(row=0, column=1)
 
         # Answer label (row 4)
@@ -69,6 +71,24 @@ class Converter:
         self.help_button = Button(self.hist_help_frame, font="arial 12 bold",
                                   text="Help", width=5)
         self.help_button.grid(row=0, column=1)
+
+        def temp_convert(self, to):
+         print(to)
+
+         # Retrieve amount entered into Entry Field
+
+         # Check amount is a valid number
+
+         # Convert to F
+
+         # Convert to C
+
+         # Round
+
+         # Display answer
+
+         # Add Answer to list for Hisotry
+
 
 
 # main routine
